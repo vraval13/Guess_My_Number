f58b77ed0d39fcef9979f5cc28952f28d8192f92
Vyom Raval <125266587+vraval13@users.noreply.github.com>
2024-05-31 17:27:12 +0530

    Update script.js

--- final/script.js
+++ final/script.js
@@ -62,17 +62,6 @@ document.querySelector('.check').addEventListener('click', function ()
     }
   }
 
-  //   // When guess is too high
-  // } else if (guess > secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = '📈 Too high!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = '💥 You lost the game!';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-
   //   // When guess is too low
   // } else if (guess < secretNumber) {
   //   if (score > 1) {
